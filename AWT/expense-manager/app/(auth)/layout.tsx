5ec55f2802b2bc89296e2262e7305711bd3731e5
RudraC-07 <rudrajchavda05@gmail.com>
2026-02-22 23:16:08 +0530

    added

--- AWT/expense-manager/app/(auth)/layout.tsx
+++ AWT/expense-manager/app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ export default function AuthLayout({
   children,
 }: any) {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen w-full">
       {children}
     </div>
   );
